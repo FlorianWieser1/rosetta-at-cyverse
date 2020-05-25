e78a64644bb9cf5688d70a62feacead50f8b7dde
flo <florian.wieser@tugraz.at>
2020-05-25 13:14:23 +0200

    Implemented flags file functionality and code layout rework for publishing

--- abinitio_container-testing/wrapper_abinitio.sh
+++ abinitio_container-testing/wrapper_abinitio.sh
@@ -3,14 +3,24 @@
 # exit immediately if a non-zero status appears
 set -e
 
-# execute rosetta protocol
 APP_PATH="$(dirname $0)/AbinitioRelax.static.linuxgccrelease"
-$APP_PATH "$@"
 
-# extract pdbs from silent.out
+# In case no "flags" or "options"-file is provided, execute Rosetta with the command-line parameters provided in the CyVerse Discovery Environment
+if [ -f "flags" ]
+then
+    $APP_PATH "@flags"
+elif [ -f "options" ]
+then
+    $APP_PATH "@options"
+else
+    $APP_PATH "$@"
+fi
+
+# Extract pdbs from silent.out (the subsequent contactMaps container requires silent.out as input)
 /working_dir/extract_pdbs.static.linuxgccrelease -in:file:silent outfile.silent.o -database /de-app-work/database_complete
 
-# post processing
+# Post processing - move computational output to a "results"-folder
+# Rename computational output in order to be recognized by the subsequent containers (for use in workflows)
 mkdir abinitio_results
 
 mv *.fsc /de-app-work/abinitio_results
