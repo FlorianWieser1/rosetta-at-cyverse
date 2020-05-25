68b34b5396f7bcfea0b07414b7eec01cbc31b69b
flo <florian.wieser@tugraz.at>
2020-05-25 17:33:45 +0200

    code layout rework

--- contact_map-container_testing/wrapper_contact-map.sh
+++ contact_map-container_testing/wrapper_contact-map.sh
@@ -3,18 +3,17 @@
 # exit immediately if a non-zero status appears
 set -e
 
-# execute rosetta protocol
 APP_PATH="$(dirname $0)/contactMap.static.linuxgccrelease"
-$APP_PATH "$@"
 
-# extract pdbs from silent.out
+# Execute Rosetta's contactMaps protocol 
 /working_dir/contactMap.static.linuxgccrelease -in:file:silent outfile.silent.o -silent_struct_type binary -database /de-app-work/database_complete
 
+# Remove unneeded header lines
 tail -n +4 *.csv | cut -f2- > outfile
 
+# Generate contactMap plot
 python /working_dir/plot-contactmap.py outfile
 
-# post processing
+# Post processing - move computational output to a "results"-folder
 mkdir contactMap_results
-
 mv contact_map* /de-app-work/contactMap_results
