d9280e7af33d3915350dbbe6f916f66a5791cb89
flo <florian.wieser@tugraz.at>
2020-05-25 10:56:27 +0200

    Added flagsfile functionality, code layout rework

--- fragment_picking-container_testing/wrapper_fragment-picking.sh
+++ fragment_picking-container_testing/wrapper_fragment-picking.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-# exit immediately if a non-zero status appears
+# Exit immediately if a non-zero status appears
 set -e
-
 
 # execute rosetta protocol
 APP_PATH="$(dirname $0)/fragment_picker.static.linuxgccrelease"
 $APP_PATH "$@"
 
-# post processing
+# In case no "flags" or "options"-file is provided, execute Rosetta with the command-line parameters provided in the CyVerse Discovery Environment
+if [ -f "flags" ]
+then
+    $APP_PATH "@flags"
+elif [ -f "options" ]
+then 
+    $APP_PATH "@options"
+else
+    $APP_PATH "$@"
+fi
+
+# Post processing - move computational output to a "results"-folder
 mkdir fragment-picker_results
 
 mv *.pdb /de-app-work/fragment-picker_results
