9563ece6bd11b709d0ddb5041f8ca5ff779c5250
flo <florian.wieser@tugraz.at>
2020-05-27 12:58:47 +0200

    got argument support in wrapper script working

--- rosetta_scripts-container_testing/wrapper_rosetta-scripts.sh
+++ rosetta_scripts-container_testing/wrapper_rosetta-scripts.sh
@@ -3,17 +3,21 @@
 # Exit immediately if a non-zero status appears
 set -e
 
-APP_PATH="$(dirname $0)/rosetta_scripts.static.linuxgccrelease"
+APP_PATH="$(dirname $0)/rosetta_scripts.static.linuxgccrelease "
 
 # In case no "flags" or "options"-file is provided, execute Rosetta with the command-line parameters provided in the CyVerse Discovery Environment
+
+# Additional flags (unsupported in the CyVerse DE due to special characters):
+PARSER_PROTOCOL=" -parser:protocol *.xml"
+
 if [ -f "flags" ]
 then
-    $APP_PATH "@flags"
+    $APP_PATH "@flags" 
 elif [ -f "options" ]
 then
     $APP_PATH "@options"
 else
-    $APP_PATH "$@"
+    $APP_PATH$@$PARSER_PROTOCOL
 fi
 
 # Post processing - move computational output to a "results"-folder
